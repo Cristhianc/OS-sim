a5834c45e847c22eda0a88748b93c3a27d47fca2
Cristhian Caicedo <cristh2592@gmail.com>
2016-11-10 10:30:10 -0430

    bankerAlgorithm.js working with no problems, almost complete

--- pro&res-admin_SIM/dist/js/bankerAlgorithm.js
+++ pro&res-admin_SIM/dist/js/bankerAlgorithm.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
       sistema asignados actualmente y de la tabla de peticiones de recursos del
       sistema por parte de los procesos, asi como el arreglo de recursos
       disponibles del sistema.*/
-      console.log(inf_proc.numProc_tabla);
       var matriz_ta = new Array(inf_proc.numProc_tabla);
       var matriz_tp = new Array(inf_proc.numProc_tabla);
       var recursos_disp = new Array(3);
@@ -14,18 +13,18 @@ $(document).ready(function(){
       }
       //Construccion del arreglo de los recursos disponibles del sistema
       for (i = 0; i < 3; i++) {
-        recursos_disp[i] = $("#rdcol"+i).val();
+        recursos_disp[i] = parseInt($("#rdcol"+i).val());
       }
       //Construccion de la matriz de recursos del sistema asignados actualmente
       for (i=0; i < inf_proc.numProc_tabla; i++) {
         for (j=0; j < 3; j++) {
-          matriz_ta[i][j] = $("#fta_"+inf_proc.nom_procs[i]+j).val();
+          matriz_ta[i][j] = parseInt($("#fta_"+inf_proc.nom_procs[i]+j).val());
         }
       }
       //Construccion de la matriz de peticiones de recursos al sistema
       for (i=0; i < inf_proc.numProc_tabla; i++) {
         for (j=0; j < 3; j++) {
-          matriz_tp[i][j] = $("#ftp_"+inf_proc.nom_procs[i]+j).val();
+          matriz_tp[i][j] = parseInt($("#ftp_"+inf_proc.nom_procs[i]+j).val());
         }
       }
 
@@ -33,48 +32,57 @@ $(document).ready(function(){
       finalizar o no. Tambien un arreglo copia de recursos_disp que permite
       la comparacion de la peticion de recursos con los recursos disponibles
       del sistema*/
-      var procs_intb = new Array(inf_proc.numProc_tabla);
-      var finalizar = new Array(3);
-      var comparar_rec = [
+      var proc_status;
+      var finalizar = new Array(inf_proc.numProc_tabla);
+      var vacum_rec = [
         recursos_disp[0],
         recursos_disp[1],
         recursos_disp[2],
         //recursos_disp[3],
         //recursos_disp[4]
       ];
-
       //Algoritmo de deteccion de interbloqueos
+      /*Primero se comprueba si todas las asignaciones de los recursos j a los
+      procesos i son iguales a 0. Si es asi, el proceso se encuentra en un es-
+      tado de falsedad; de lo contrario, el proceso se encuentra en un estado
+      de verdad.*/
       for (i = 0; i < inf_proc.numProc_tabla; i++) {
         for (j = 0; j < 3; j++) {
-          /*Verifica si hay algun recurso del tipo j asignado al proceso i, de
-          ser asi, indica que todavia no ha finalizado. De lo contrario, indica
-          que si ha finalizado.*/
-          if (matriz_ta[i][j]===0) {
-            finalizar[j] = true;
+          if (matriz_ta[i][j] != 0) {
+            finalizar[i] = false;
+            break;
           }
           else {
-            finalizar[j] = false;
-          }
-          /*Luego verifica si hay algun proceso i con un recurso j faltante a
-          asignar debido a una peticion del mismo al sistema y si, tambien los
-          recursos pedidos son menores o iguales a los disponibles. De ser asi,
-          devuelve los recursos al sistema y da por finalizado el proceso.
-
-          De lo contrario, se sale de la evaluacion del proceso i y se determi-
-          na que ese proceso esta interbloqueado.*/
-          if (finalizar[j] === false && matriz_tp[i][j] <= comparar_rec[j]) {
-            comparar_rec[j] += matriz_ta[i][j];
-            finalizar[j] = true;
-          }
-          else {
-            j=-1;
-            $("#activ_"+inf_proc.nom_procs).remove();
-            $("#tabla_asignados").append('<tr id="fta_'+inf_proc.nom_procs[i]+'">Bloqueado</tr>');
+            finalizar[i] = true;
           }
         }
-        if (j !== -1) {
-          $("#activ_"+inf_proc.nom_procs).remove();
-          $("#tabla_asignados").append('<tr id="fta_'+inf_proc.nom_procs[i]+'">Finalizado</tr>');
+      }
+      /*Luego se procede a comparar si el vector fila de las peticiones del
+      recurso j por parte del proceso i es menor o igual que el vector de acu-
+      mulacion de recursos hasta el momento. Si lo es, entonces se realiza la
+      suma entre estos vectores; de lo contrario, se determina que hubo un
+      interbloqueo al proceso i.*/
+      for (i = 0; i < inf_proc.numProc_tabla; i++) {
+        proc_status=0;
+        if (finalizar[i] != true) {
+          for (j = 0; j < 3; j++) {
+            if (!(matriz_tp[i][j] <= vacum_rec[j])) {
+              console.log("matriz_tp["+i+","+"]:"+matriz_tp[i][j]);
+              console.log("vacum_rec["+j+"]:"+vacum_rec[j]);
+              proc_status=1;
+              $("#activ_"+inf_proc.nom_procs[i]).remove();
+              $("#fta_"+inf_proc.nom_procs[i]).append('<th id="fta_'+inf_proc.nom_procs[i]+'5">Bloqueado</th>');
+              break;
+            }
+          }
+        }
+        if (proc_status == 0) {
+          $("#activ_"+inf_proc.nom_procs[i]).remove();
+          $("#fta_"+inf_proc.nom_procs[i]).append('<th id="fta_'+inf_proc.nom_procs[i]+'5">Finalizado</th>');
+          for (j=0; j < 3; j++) {
+            vacum_rec[j] += matriz_ta[i][j];
+            console.log("vacum_rec["+j+"]:"+vacum_rec[j]);
+          }
         }
       }
     });
